0fb33a66f1fa11016e74f485f28ed127ca6e5c87
Nicholas Glesmann <nicholasglesmann@gmail.com>
2020-05-27 16:02:19 -0700

    Bug fix

--- dist/src/js/main.js
+++ dist/src/js/main.js
@@ -28,7 +28,7 @@ function toggleMenu() {
 
 window.onload = function () {
     animateElements();
-    //adjustHomePageMobileSize();
+    adjustHomePageMobileSize();
 };
 
 function animateElements() {
